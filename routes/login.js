9c7b4ad917c81e245498fc5d5c785430eefb434b
unknown <saket.pakhale1002@gmail.com>
2024-05-25 15:24:08 +0530

    removed unecessary files, login.js and signup.js

--- routes/login.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const config = require('../config/config');
-const jwt = require('jsonwebtoken');
-const { auth } = require("./middleware");
-const JWT_SECRET = config.JWT_SECRET;
-
-
-module.exports = router;
